0222d73c0326bfcc6b20a170bd126fc760441e67
Krevilcio <sieniewski@gmail.com>
2024-09-06 02:21:04 +0200

    Task5

---
+++ 1_Week/Task5/Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿bool running = true;
+
+while (running)
+{
+    Console.WriteLine("Enter employee name:");
+    string name = Console.ReadLine();
+    
+    Console.WriteLine("Enter employee age:");
+    int age = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine("Enter a function: (Driver, Mechanic, Personnel)");
+    string function = Console.ReadLine();
+    
+    Employee employee = new Employee(name, age, function);
+    
+    double salary1 = employee.GetSalary1();
+    double salary2 = employee.GetSalary2();
+    double salary3 = employee.GetSalary3();
+    
+    
+    Console.WriteLine($"Employee {name} salary: 1. {salary1} 2. {salary2} 3. {salary3} as {function}");
+    
+    Console.WriteLine("Do you want to add another employee? (y/n)");
+    string answer = Console.ReadLine();
+
+    if (answer.ToLower() != "y")
+    {
+        running = false;
+    }
+}
---
+++ 1_Week/Task5/Task5/employee.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+class Employee
+{
+    public string Name;
+    public int Age;
+    public string Function;
+   
+    
+    public Employee(string name, int age, string function)
+    {
+        Name = name;
+        Age = age;
+        Function = function;
+    }
+    
+    
+    
+    public double GetSalary1()
+    {
+        double baseSalary = 0;
+
+        if (Function == "Driver")
+        {
+            baseSalary = 6000;
+        }
+        else if (Function == "Mechanic")
+        {
+            baseSalary = 5000;
+        }
+        else if (Function == "Personnel")
+        {
+            baseSalary = 4000;
+        }
+        else
+        {
+            Console.WriteLine("Unable to calculate salary");
+        }
+        
+        return Math.Round(baseSalary * 1.1 * Age, 2);
+    }
+    
+    
+
+    public double GetSalary2()
+    {
+        double baseSalary = 0;
+
+        if (Function == "Driver")
+        {
+            baseSalary = 6000;
+        }
+        else if (Function == "Mechanic")
+        {
+            baseSalary = 5000;
+        }
+        else if (Function == "Personnel")
+        {
+            baseSalary = 4000;
+        }
+        else
+        {
+            Console.WriteLine("Unable to calculate salary");
+        }
+        
+        return Math.Round(baseSalary * 2.0 * Age, 2);      
+    }
+    
+    public double GetSalary3()
+    {
+        double baseSalary = 0;
+
+        if (Function == "Driver")
+        {
+            baseSalary = 6000;
+        }
+        else if (Function == "Mechanic")
+        {
+            baseSalary = 5000;
+        }
+        else if (Function == "Personnel")
+        {
+            baseSalary = 4000;
+        }
+        else
+        {
+            Console.WriteLine("Unable to calculate salary");
+        }
+        
+        return Math.Round(baseSalary * 3.5 * Age, 2);       
+        
+    }
+
+}
